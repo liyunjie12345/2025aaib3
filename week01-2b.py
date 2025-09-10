d54957c15baf0adb0cdcded940893e234189d960
liyunjie12345 <wyz941231@gmail.com>
2025-09-10 11:50:59 +0800

    week01

---
+++ week01-2b.py
@@ -0,0 +1,5 @@
+# week01-2b.py LeetCode 學習計畫 Python 版本2 使用字串的find()函式
+# LeetCode 28. Find the Index of the First Occurrence in a string
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle) # 只有一行
